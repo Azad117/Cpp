96fb82bc021016e5f28659b5be6cfadd5a29d537
J.VenuGopal Azad <95510274+Azad117@users.noreply.github.com>
2023-06-18 16:34:25 +0530

    Create Lab3.cpp

---
+++ Lab3.cpp
@@ -0,0 +1,24 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int n,i=1;
+	clrscr();
+	cout<<"Enter n value \n";
+	cin>>n;
+	while(i<=n)
+	{
+		cout<<i<<" ";
+		i++;
+	}
+	do
+	{
+		cout<<i<<" ";
+		i++;
+	}while(i<=n);
+ for(i=1;i<=n;i++)
+	{
+		cout<<i<<" ";
+	}
+  getch();
+ }
